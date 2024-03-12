e3800b8a6483ece781184b2b4d19684e54aa4292
abrea.prog <aru.brea@gmail.com>
2024-03-12 17:05:38 -0300

    sigo probando

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import login from './Components/Login/login'
+import LoginPage from './Components/Login/login'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,7 +9,7 @@ function App() {
     <>
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<login />} />
+        <Route path="/login" element= {<LoginPage />} />
       </Routes>
     </BrowserRouter>
   </>
--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -1,4 +1,17 @@
-const login = ()=>{
+const LoginPage = ()=>{
+
+    return (
+        <form className="form-login">
+           <input placeholder="nombre"></input> 
+           <input placeholder="apellido"></input>
+           <input placeholder="correo electrónico"></input>
+           <input placeholder="contraseña"></input>
+           <input placeholder="repetir contraseña"></input>
+           <select placeholder="localidad"></select>
+           <button>Registrar</button>
+
+        </form>
+    )
 
 }
-export default login;
+export default LoginPage;
