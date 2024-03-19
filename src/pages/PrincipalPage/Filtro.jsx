16d33ddf2a3597885243b88c6f714927445e0fa8
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-03-19 12:41:56 -0500

    filtros combinados 10/10 Sebastian

--- src/pages/PrincipalPage/Filtro.jsx
+++ src/pages/PrincipalPage/Filtro.jsx
@@ -4,7 +4,7 @@ import { getAllAlojamientos } from '../../Redux/boscoSlice'
 import { useDispatch } from "react-redux";
 
 import axios from 'axios';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useServices } from '../../Hooks/useServices';
 
 export const Filtros = () => {
@@ -12,32 +12,34 @@ export const Filtros = () => {
     useServices();
     const dispatch = useDispatch();
 
-
-    const [filter, setFilter] = useState({
+    const initialState = {
         location: "",
-        serviceId: [],
-        square: "",
+        serviceId: "",
+        square: 1,
         maxPrice: "",
         startDate: "",
         endDate: "",
         orderBy: "",
         orderDirection: "",
-    })
+    }
+
+
+    const [filter, setFilter] = useState(initialState)
 
     const URL = 'http://localhost:3001/profileHousing/filtered'
     
     const provincias = useSelector((state) => state.storage.AllLocation);
     const services = useSelector((state) => state.storage.AllService);
+    const Alojamiento = useSelector((state) => state.storage.allAlojamientos)
 
     const handleChange = async (e) => {
+        const changeFilter = {...filter, [e.target.name]: e.target.value}
+        setFilter(changeFilter)
         let query = "?";
 
-        for (const [key, value] of Object.entries({
-            ...setFilter,
-            [e.target.name]: e.target.value,
-          })) {
+        for (const [key, value] of Object.entries(changeFilter)) {
             if (value) query += `${key}=${value}&`;
-          }
+        }
 
           try {
             const { data } = await axios.get(URL + query);
@@ -45,63 +47,93 @@ export const Filtros = () => {
           } catch (error) {
             console.log(error);
           }
-
-
-
     }
 
-    // const responseAlojamientos = async() => {
-    //     try {
-    //         const response = await axios() 
 
+    const [show, setShow] = useState(true)
 
-    //     } catch (error) {
-            
-    //     }
-    // }
+    const resetFilter = async () => {
+        setFilter(initialState)
+
+        if(show) setShow(false)
+
+        try {
+            const { data } = await axios.get(URL);
+            dispatch(getAllAlojamientos(data));
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    useEffect(() => {
+        setShow(true)
+    }, [Alojamiento.length])
+
+    
 
     return (
         <div className="w-[400px] bg-orange-600 h-[100vh] ">
-            <select onChange={handleChange} name="location">
-                <option>Escoge una provincia</option>
-                {provincias.map((provincia) => {
-                    return <option value={provincia.nombre} key={provincia.id}>{provincia.nombre}</option>
-                })}
 
-            </select>
+            {show && <div>
+                <select onChange={handleChange} name="location">
+                    <option value="">Escoge una provincia</option>
+                    {provincias.map((provincia) => {
+                        return <option value={provincia.nombre} key={provincia.id}>{provincia.nombre}</option>
+                    })}
 
-            <select onChange={handleChange} name="serviceId">
-                <option>Services</option>
-                {services.map((service) => {
-                    return <option value={service.id} key={service.id}>{service.type}</option>
-                })}
-            </select>
+                </select>
+
+                <select onChange={handleChange} name="serviceId">
+                    <option value="">Services</option>
+                    {services.map((service) => {
+                        return <option value={service.id} key={service.id}>{service.type}</option>
+                    })}
+                </select>
 
 
-            
-            <div>
-                <label htmlFor="square" >plazas</label>
-                <input type="number" 
-                    id="square" name="square" min="1" max="20" 
-                    onChange={handleChange}
-                    value={filter.square}
-                />
-            </div> 
+                
+                <div>
+                    <label htmlFor="square" >plazas</label>
+                    <input type="number" 
+                        id="square" name="square" min="1" max="20" 
+                        onChange={handleChange}
+                        value={filter.square}
+                        defaultValue={1}
+                    />
+                </div> 
 
-            {/* <div>
-                <input value={filter.square} name='square' id='square' type='range'
-                        min="5"
-                        max="100"
-                        onChange={handleChange} 
-                ></input>
-            </div>
+                <div>
+                    <label htmlFor="maxPrice" >Precio</label>
+                    <input value={filter.maxPrice} name='maxPrice' id='maxPrice' type='range'
+                            min="5"
+                            max="100"
+                            step="5"
+                            onChange={handleChange} 
+                    ></input>
+                    {<span>${filter.maxPrice}</span>}
+                </div>
 
-            <div>
-            <label>plazas</label>
-                <input value={filter.square} name='square' id='square'
-                    onChange={handleChange} 
-                ></input>
-            </div> */}
+                <div>
+                    <label>Orden</label>
+                    <select name='orderBy' onChange={handleChange}>
+                        <option value="">Escoge un tipo orden</option> 
+                        <option value="price">Precio</option>
+                        <option value="square">Plazas</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label>Dirección Orden</label>
+                    <select name='orderDirection' onChange={handleChange}>
+                        <option value="">Escoge una dirección de orden</option>
+                        <option value="DESC">DESCENDENTE</option>
+                        <option value="ASC">ASCEDENTE</option>
+                    </select>
+                </div>
+
+            </div>}
+
+                <button onClick={resetFilter}>Resetear Filtros</button>
         </div>
     )
 }
