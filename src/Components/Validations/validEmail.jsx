d10c109c06b67b3b70ce94b1b4762352dfa02fa5
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-03-23 14:10:29 -0500

    correcion de validacion

--- src/Components/Validations/validEmail.jsx
+++ src/Components/Validations/validEmail.jsx
@@ -17,7 +17,9 @@ export const isValidEmail = async (email) => {
         }
 
         // Obtener todos los usuarios de la base de datos
-        const response = await axios.get('/user');
+        let response = await axios.get('/user');
+
+        if(response === "No se encontraron usuarios") response.data = []
         const users = response.data;
 
 
