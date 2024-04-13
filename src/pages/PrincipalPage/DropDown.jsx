999b53cada55e8c2204637bd24fa506caa58edfa
GIrigoin <guilleirigoin@hotmail.com>
2024-04-13 20:33:44 -0300

    arreglos varios sobre todo el form alojamiento que se habia hecho pedazos

--- src/pages/PrincipalPage/DropDown.jsx
+++ src/pages/PrincipalPage/DropDown.jsx
@@ -1,27 +1,27 @@
-import { Fragment } from 'react'
-import { Menu, Transition } from '@headlessui/react'
-import { useNavigate } from 'react-router-dom'
-
-
+import { Fragment } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { useNavigate } from "react-router-dom";
+import defaultProfile from "../../assets/perfilPicture.webp";
 
 function classNames(...classes) {
-  return classes.filter(Boolean).join(' ')
+  return classes.filter(Boolean).join(" ");
 }
 
 export default function DropDown() {
+  const usuario = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
   return (
     <Menu as="div" className="relative inline-block text-left">
       <div>
-      <Menu.Button className="relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                      <span className="absolute -inset-1.5" />
-                      <span className="sr-only">Open user menu</span>
-                      <img
-                        className="h-8 w-8 rounded-full"
-                        src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                        alt=""
-                      />
-                    </Menu.Button>
+        <Menu.Button className="relative flex rounded-full text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+          <span className="absolute -inset-1.5" />
+          <span className="sr-only">Open user menu</span>
+          <img
+            className="h-8 w-8 rounded-full"
+            src={usuario?.picture || defaultProfile}
+            alt=""
+          />
+        </Menu.Button>
       </div>
 
       <Transition
@@ -40,8 +40,8 @@ export default function DropDown() {
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-sm"
                   )}
                 >
                   Account settings
@@ -53,8 +53,8 @@ export default function DropDown() {
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-sm"
                   )}
                 >
                   Support
@@ -66,39 +66,34 @@ export default function DropDown() {
                 <a
                   href="#"
                   className={classNames(
-                    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                    'block px-4 py-2 text-sm'
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block px-4 py-2 text-sm"
                   )}
                 >
                   License
                 </a>
               )}
             </Menu.Item>
-              <Menu.Item>
-                {({ active }) => (
-                  <button
-                    type="submit"
-                    className={classNames(
-                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-                      'block w-full px-4 py-2 text-left text-sm'
-                    )}
-
-                    onClick={() => {
-                         localStorage.removeItem("user");
-                         navigate('/')
-                    }}
-                  >
-                    Sign out
-                  </button>
-                )}
-              </Menu.Item>
+            <Menu.Item>
+              {({ active }) => (
+                <button
+                  type="submit"
+                  className={classNames(
+                    active ? "bg-gray-100 text-gray-900" : "text-gray-700",
+                    "block w-full px-4 py-2 text-left text-sm"
+                  )}
+                  onClick={() => {
+                    localStorage.removeItem("user");
+                    navigate("/");
+                  }}
+                >
+                  Sign out
+                </button>
+              )}
+            </Menu.Item>
           </div>
         </Menu.Items>
       </Transition>
     </Menu>
-  )
+  );
 }
-
-
-
-
