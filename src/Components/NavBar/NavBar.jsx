43c1e806feb600a3998854d06ef1aca9875acb75
GIrigoin <guilleirigoin@hotmail.com>
2024-04-19 14:13:22 -0300

    Arreglillos varios

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import Isologotipo from "../../assets/IsoLogotipoBosco.png";
-import pictureDefault from "../../assets/perfilPicture.webp"
+import pictureDefault from "../../assets/perfilPicture.webp";
 
 export const Navbar = () => {
   const nameUsuario = JSON.parse(localStorage.getItem("user"));
-  let picture = pictureDefault
+  let picture = pictureDefault;
 
   if (JSON.parse(localStorage.getItem("user"))) {
-    if(JSON.parse(localStorage.getItem("user")).picture) {
-      picture = JSON.parse(localStorage.getItem("user")).picture
+    if (JSON.parse(localStorage.getItem("user")).picture) {
+      picture = nameUsuario.picture.picture;
     }
   }
   return (
@@ -29,57 +29,58 @@ export const Navbar = () => {
         <div className="w-[519.5px] flex flex-row items-start justify-start gap-[0px_101.9px] max-w-full text-mini-5 mq450:gap-[0px_25px] mq900:w-[354.9px] mq900:gap-[0px_51px]">
           <nav className="m-0 h-[42.1px] flex-1 flex flex-row items-start justify-start text-left text-mini-8 text-midnightblue font-inter">
             <div className="self-stretch w-[127.4px] rounded-181xl flex flex-col items-start justify-start pt-[11.100000000000025px] pb-[10.999999999999972px] pr-[7.5px] pl-[49.89999999999998px] box-border z-[4] ml-[-24px] text-mini-9">
-              <NavLink
+              {/* <NavLink
                 className="flex-1 relative leading-[20px] no-underline"
                 to="/RegisterCompany"
               >
                 Empresas
-              </NavLink>
+              </NavLink> */}
             </div>
 
             <div className="self-stretch w-[127.4px] rounded-181xl flex flex-col items-start justify-start pt-[11.100000000000025px] pb-[10.999999999999972px] pr-[7.5px] pl-[49.89999999999998px] box-border z-[4] ml-[-24px] text-mini-9">
-              <NavLink className="flex-1 relative leading-[20px] no-underline">
+              {/* <NavLink className="flex-1 relative leading-[20px] no-underline">
                 Contacto
-              </NavLink>
+              </NavLink> */}
             </div>
           </nav>
 
           <div className="w-[164.6px] flex flex-row items-start justify-start gap-[0px_29.8px] mq900:hidden">
+            {nameUsuario?.email ? (
+              <div>
+                <button className="flex flex-col items-start w-[45px] h-[45px] justify-start pt-[7.899999999999977px] px-0 pb-0">
+                  <NavLink
+                    to="/Profile/perfil"
+                    className="cursor-pointer relative leading-[28.13px] whitespace-nowrap z-[3] no-underline"
+                  >
+                    <img
+                      className="w-full h-full p-0 border rounded-md bg-slate-300 "
+                      src={picture}
+                      alt=""
+                    />
+                  </NavLink>
+                </button>
+              </div>
+            ) : (
+              <>
+                <button className="flex flex-col items-start justify-start pt-[7.899999999999977px] px-0 pb-0">
+                  <NavLink
+                    to="/Register"
+                    className="cursor-pointer relative leading-[28.13px] whitespace-nowrap z-[3] no-underline"
+                  >
+                    Sign up
+                  </NavLink>
+                </button>
 
-            {
-              nameUsuario?.email ?
-                <div>
-
-                  <button className="flex flex-col items-start w-[45px] h-[45px] justify-start pt-[7.899999999999977px] px-0 pb-0">
-                    <NavLink
-                      to="/Profile/perfil"
-                      className="cursor-pointer relative leading-[28.13px] whitespace-nowrap z-[3] no-underline"
-                    >
-                          <img className="w-full h-full p-0 border rounded-md bg-slate-300 " src={pictureDefault} alt="" />
-                    </NavLink>
-                  </button>
-                </  div>
-                :
-                <>
-                  <button className="flex flex-col items-start justify-start pt-[7.899999999999977px] px-0 pb-0">
-                    <NavLink
-                      to="/Register"
-                      className="cursor-pointer relative leading-[28.13px] whitespace-nowrap z-[3] no-underline"
-                    >
-                      Sign up
-                    </NavLink>
-                  </button>
-
-                  <button className="cursor-pointer [border:none] py-3 pr-[20.799999999999955px] pl-[21px] bg-chocolate-100 flex-1 rounded-181xl flex flex-row items-start justify-start whitespace-nowrap z-[3] hover:bg-chocolate-200">
-                    <NavLink
-                      to="/login"
-                      className="cursor-pointer no-underline flex-1 relative text-mini-3 leading-[20px] font-medium font-inter text-white text-center"
-                    >
-                      Log In
-                    </NavLink>
-                  </button>
-
-                </>}
+                <button className="cursor-pointer [border:none] py-3 pr-[20.799999999999955px] pl-[21px] bg-chocolate-100 flex-1 rounded-181xl flex flex-row items-start justify-start whitespace-nowrap z-[3] hover:bg-chocolate-200">
+                  <NavLink
+                    to="/login"
+                    className="cursor-pointer no-underline flex-1 relative text-mini-3 leading-[20px] font-medium font-inter text-white text-center"
+                  >
+                    Log In
+                  </NavLink>
+                </button>
+              </>
+            )}
           </div>
         </div>
       </div>
