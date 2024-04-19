b2b30d26cf0506f5bfb0f5572f066988d27b01ff
Sebastian Agudelo <agudelojoan28@gmail.com>
2024-04-19 09:50:56 -0500

    cambios chiquitos

--- src/pages/PrincipalPage/DropDown.jsx
+++ src/pages/PrincipalPage/DropDown.jsx
@@ -38,7 +38,7 @@ export default function DropDown() {
             <Menu.Item>
               {({ active }) => (
                 <a
-                  href="#"
+                  href="/Profile/perfil"
                   className={classNames(
                     active ? "bg-gray-100 text-gray-900" : "text-gray-700",
                     "block px-4 py-2 text-sm"
